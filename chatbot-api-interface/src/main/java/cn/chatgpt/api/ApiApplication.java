8c2f843f6a0d8127fbd1ba50f9596b2510c149fb
雷义焘 <8231658+lei-yitao@user.noreply.gitee.com>
2023-03-19 00:01:50 +0800

    初始化工程

---
+++ chatbot-api-interface/src/main/java/cn/chatgpt/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package cn.chatgpt.api;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Descrpition 项目启动入口
+ */
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
